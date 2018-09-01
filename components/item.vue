bba1b33eebc7d671f3b35d1ababc27943032b96e
Steven <qinyuyao02@qq.com>
2018-09-01 16:34:52 +0800

    push

---
+++ components/item.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="daily-item">
+		<div class="daily-img" v-if="data.images">
+			<img :src="imgPath + data.images[0]" alt="">
+		</div>
+		<div class="daily-title" 
+			:class="{ noImg: !data.images}">
+			{{ data.title }}			
+		</div>
+	</div>
+</template>
+
+<script>
+	import $ from '../libs/util.js';
+	export default {
+		props: {
+			data: {
+				type: Object
+			}
+		},
+		data () {
+			return {
+				imgPath: $.imgPath
+			}
+		},
+	}
+</script>
+
+
